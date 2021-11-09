71862b351344388240f5d01e5a34ab8e76ff71c6
Enoch2k2 <enoch2k2@hotmail.com>
2021-11-09 10:38:50 -0600

    added routing to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
